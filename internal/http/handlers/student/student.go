b239ff40521c62ccf050c21fdeda0e1ee7e2ca69
Abhishek Solanki <abhisheksolanki01010@gmail.com>
2025-03-20 19:31:25 +0530

    add get students list

--- internal/http/handlers/student/student.go
+++ internal/http/handlers/student/student.go
@@ -83,3 +83,18 @@ func GetById(storage storage.Storage) http.HandlerFunc {
 		response.WriteJson(w, http.StatusOK, student)
 	}
 }
+
+func GetList(storage storage.Storage) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		slog.Info("Getting all students")
+
+		students, err := storage.GetStudents()
+		if err != nil {
+			slog.Error("error getting students", slog.String("Error", err.Error()))
+			response.WriteJson(w, http.StatusInternalServerError, response.GeneralError(err))
+			return
+		}
+		response.WriteJson(w, http.StatusOK, students)
+	}
+}
